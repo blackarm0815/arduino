3b7e7af5d53244b5d63e5e6b4a80c524b89e4845
blackarm0815 <honungsbjorn@protonmail.com>
2023-03-20 15:04:17 +0000

    :)

--- timer_servo/timer_servo.ino
+++ timer_servo/timer_servo.ino
@@ -1,78 +1,43 @@
-// the length of the animation in milliseconds.
-const int animationLength = 8000L;
-// when the button is pressed, this is made equal to the current time. the animation starts from there.
-int unsigned long timerPoint = 0L;
-// this is made true when the button is pressed, to avoid an animation happening when the arduino turns on.
-bool timerActive = false;
-// the number of milliseconds into the animation. if the animation is not running, it will be zero.
-int animationTime = 0;
-// position of the server from 0 to 180
-int long servoPosition = 0L;
+int animationTimer = 0;
+int servoPosition = 0;
 
 void setup() {
-  // set up pin 2 for the button
   pinMode(2, INPUT);
-  // activate the serial monitor
   Serial.begin(9600);
 }
 
 void buttonCheck() {
-  // if the button is pressed
   if (digitalRead(2) == HIGH) {
-    // move the timerPoint to match the current time, starting the animation
-    timerPoint = millis();
-    // making this true allows animation calculations to happen
-    timerActive = true;
+    animationTimer = 1;
   } 
 }
 
-int calcAnimationTime() {
-  // before the button is pressed, always return zero  
-  if (!timerActive) {
-    return 0;
-  }
-  if (millis() > (timerPoint + animationLength)) {
-    return 0;
-  }
-  // calculate the animationTime between the start time and current time    
-  int calcTime = millis() - timerPoint;
-  // if the calcTime is greater than the length of the animation return zero. it is over.
-  if (calcTime > animationLength) {
-    calcTime = 0;
-  }
-  // if the code gets this far, the animation is still in progress, return the 
-  return calcTime;
-}
-
 int calcServoPosition() {
-  int long foo = 0;
-  // move to 180 for the first half
-  if (animationTime < (animationLength / 2)) {
-    foo = ((animationTime * 360L) / animationLength);
+  if (animationTimer == 0) {
+    return 0;
   }
-  // move from 180 back to 0 for the second half
-  else {
-    foo = ((animationLength - animationTime) * 360L / animationLength);
+  if (animationTimer > 0 && animationTimer < 400) {
+    animationTimer += 1;
+    return map(animationTimer, 0, 400, 0, 180);
   }
-  return foo;
+  if (animationTimer >= 400 && animationTimer < 800) {
+    animationTimer += 1;
+    return map(animationTimer, 400, 800, 180, 0);
+  }
+  if (animationTimer == 800) {
+    animationTimer = 0;
+    return 0;
+  }
 }
 
 void showValues() {
-  Serial.print(timerActive);
-  Serial.print(' ');
-  Serial.print(millis());
-  Serial.print(' ');
-  Serial.print(timerPoint);
-  Serial.print(' ');
-  Serial.print(animationTime);
-  Serial.print(' ');
-  Serial.print(servoPosition);
-  Serial.print("\n");
+  char buffer[10];
+  sprintf(buffer, "%03d %03d\n", animationTimer, servoPosition);
+  Serial.write(buffer);
 }
 
 void loop() {
   buttonCheck();
-  animationTime = calcAnimationTime();
   servoPosition = calcServoPosition();
   showValues();
 }
