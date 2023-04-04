659442779a93365e4582b304174249a4ae037c2b
blackarm0815 <honungsbjorn@protonmail.com>
2023-04-04 01:16:27 +0100

    :)

--- animation_new/animation_new.ino
+++ animation_new/animation_new.ino
@@ -1,11 +1,18 @@
+// animation states
+//
+// 0 at the start of the forward animation. servo deactivated.
+// 1 when the forward animation is in progress.
+// 2 at the start of the backwards animation. servo deactivated.
+// 3 when the backwards animation is in progress
+
 #include <Servo.h>
 Servo servoAlpha;
 Servo servoBeta;
-int forwardAnimation = 0;
-int servoPositionAlpha = 0;
-int backAnimation = 0;
-int servoPositionBeta = 0;
+int animationCycleBackward = 0;
+int animationCycleForward = 0;
 int animationStage = 0;
+int servoPositionAlpha = 0;
+int servoPositionBeta = 0;
 bool wasButtonLow = true;
 
 void setup() {
@@ -16,130 +23,116 @@ void setup() {
 }
 
 void incrementForwardAnimation() {
-  if (forwardAnimation > 0) {
-    forwardAnimation += 1;
+  if (animationCycleForward < 300) {
+    animationCycleForward += 1;
   }
-  if (forwardAnimation == 300) {
-    forwardAnimation = 0;
-  }
-}
-
-
-void incrementBackAnimation() {
-  if (backAnimation > 0) {
-    backAnimation += 1;
-  }
-  if (backAnimation == 300) {
-    backAnimation = 0;
-  }
-}
-
-
-void showValues() {
-  char buffer[30];
-  
-  if (wasButtonLow) {
-    sprintf(buffer, "%s %03d %06d %03d %03d %03d\n", "true ", animationStage, forwardAnimation, servoPositionAlpha, backAnimation, servoPositionBeta);
-
-  }
-
   else {
-    sprintf(buffer, "%s %03d %06d %03d %03d %03d\n", "false", animationStage, forwardAnimation, servoPositionAlpha, backAnimation, servoPositionBeta);
-
+    animationStage = 2;
+    servoAlpha.detach(10);
+    servoBeta.detach(11);
   }
-    Serial.write(buffer);
 }
-
 
 int alphaForward() {
-  
-  if (forwardAnimation > 0 && forwardAnimation < 200) {
-    return map(forwardAnimation, 0, 200, 0, 180);
+  if (animationCycleForward > 0 && animationCycleForward < 200) {
+    return map(animationCycleForward, 0, 200, 0, 180);
   }
-
-  if (forwardAnimation >= 201 && forwardAnimation < 300) {
+  if (animationCycleForward >= 201 && animationCycleForward < 300) {
     return 180;
   }
 }
 
 int betaForward() {
-
-  if (forwardAnimation >= 0 && forwardAnimation < 200) {
+  if (animationCycleForward >= 0 && animationCycleForward < 200) {
     return 0;
   }
-  if (forwardAnimation >= 201 && forwardAnimation < 300) {
-    return map(forwardAnimation, 201, 300, 0, 90);
+  if (animationCycleForward >= 201 && animationCycleForward < 300) {
+    return map(animationCycleForward, 201, 300, 0, 90);
   }
 }
 
-
-
+void incrementBackAnimation() {
+  if (animationCycleBackward < 300) {
+    animationCycleBackward += 1;
+  }
+  else {
+    animationStage = 0;
+    servoAlpha.detach(10);
+    servoBeta.detach(11);
+  }
+}
 
 int alphaBack() {
-  if (backAnimation >= 0 && backAnimation < 100) {
+  if (animationCycleBackward >= 0 && animationCycleBackward < 150) {
     return 180;
   }
-  if (backAnimation >= 101 && backAnimation < 300) {
-    return map(backAnimation, 101, 300, 180, 0);
+  if (animationCycleBackward >= 151 && animationCycleBackward < 300) {
+    return map(animationCycleBackward, 151, 300, 180, 0);
   }
 }
 
 int betaBack() {
-  if (backAnimation > 0 && backAnimation < 100) {
-    return map(backAnimation, 0, 100, 90, 0);
+  if (animationCycleBackward > 0 && animationCycleBackward < 150) {
+    return map(animationCycleBackward, 0, 150, 90, 0);
   }
-  if (backAnimation >= 101 && backAnimation < 300) {
+  if (animationCycleBackward >= 151 && animationCycleBackward < 300) {
     return 0;
   }
- 
 }
 
+void showValues() {
+  char buffer[200];
+  sprintf(buffer, "animationStage %d animationCycleForward %03d servoPositionAlpha %03d animationCycleBackward %03d servoPositionBeta %03d\n", animationStage, animationCycleForward, servoPositionAlpha, animationCycleBackward, servoPositionBeta);
+  Serial.write(buffer);
+}
 
-
-
-void startAnimationButton() {
-
- if (digitalRead(2) == LOW) {
-  wasButtonLow = true;
+void buttonPressed() {
+  // trigger the forward animation
+  if (animationStage == 0) {
+    animationStage = 1;
+    animationCycleForward = 0;
+    servoAlpha.attach(10);
+    servoBeta.attach(11);
   }
-  
+  // trigger the backward animation
+  if (animationStage == 2) {
+    animationStage = 3;
+    animationCycleBackward = 0;
+    servoAlpha.attach(10);
+    servoBeta.attach(11);
+  }
+  // if the button is pressed while an animation is in progress (1 or 3) it is ignored
+}
+
+void checkAnimationButton() {
+  if (digitalRead(2) == LOW) {
+    wasButtonLow = true;
+  }
   if (digitalRead(2) == HIGH && wasButtonLow) {
     wasButtonLow = false;
-      if (animationStage == 0) {
-        animationStage = 1;
-        forwardAnimation = 1;
-        backAnimation = 0;
-      } 
-      
-      else {
-        animationStage = 0;
-        forwardAnimation = 0;
-        backAnimation = 1; 
-      }
-    }
-  } 
+    buttonPressed();
+  }
+}
 
 void animation(){
-  startAnimationButton();
-  if (animationStage == 1 && forwardAnimation > 0) {
-     servoPositionAlpha = alphaForward();
-    
-     servoPositionBeta = betaForward();
-    
-    } 
-    if (animationStage == 0 && backAnimation > 0) {
-      servoPositionAlpha = alphaBack();
-      
-      servoPositionBeta = betaBack();
-      }
-  servoAlpha.write(servoPositionAlpha);
-  servoBeta.write(servoPositionBeta);
-  
-  
-  showValues();  
-  
-  incrementForwardAnimation();
-  incrementBackAnimation();
+  checkAnimationButton();
+  // forward animation
+  if (animationStage == 1) {
+    incrementForwardAnimation();
+    servoPositionAlpha = alphaForward();
+    servoPositionBeta = betaForward();
+    servoAlpha.write(servoPositionAlpha);
+    servoBeta.write(servoPositionBeta);
+  }
+  // backward animation
+  if (animationStage == 3) {
+    incrementBackAnimation();
+    servoPositionAlpha = alphaBack();
+    servoPositionBeta = betaBack();
+    servoAlpha.write(servoPositionAlpha);
+    servoBeta.write(servoPositionBeta);
+  }
+  showValues();
 }
 
 void loop() {
