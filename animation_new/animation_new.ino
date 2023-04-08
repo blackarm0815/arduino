1bf8bcccf209962ed454ca663d94bc6643666ceb
blackarm0815 <honungsbjorn@protonmail.com>
2023-04-08 22:05:40 +0100

    added notes

--- animation_new/animation_new.ino
+++ animation_new/animation_new.ino
@@ -1,42 +1,47 @@
 // animation states
 //
+
+// the servo library
+#include <Servo.h>
+// create the servo objects
+Servo servoAlpha;
+Servo servoBeta;
+// this keeps track of when the button was not pressed is used to make sure that the button actions do not happen every cycle
+// only on the first cycle the button is pressed, when wasButtonLow is still true
+bool wasButtonLow = true;
 // 0 starting position.
 // 1 forward animation.
 // 2 stopped at end of forward animation.
 // 3 backwards animation.
-
-#include <Servo.h>
-Servo servoAlpha;
-Servo servoBeta;
-
-bool wasButtonLow = true;
-
 int animationStage = 0;
-
+// the cycle counts for the forward and backward animations
 int animationCycleBackward = 0;
 int animationCycleForward = 0;
-
+// variables for storing the servos angles in degrees
 int servoPositionAlpha = 0;
 int servoPositionBeta = 0;
-
-int cycleMax = 32000;
-
+// the full length of the animation in cycles
+// an unsigned int is used because it has a maximum value of 65,535
+// unlike a regular int that has a range of -32,768 to 32,767
+unsigned int cycleMax = 50000;
+//
 void setup() {
+  // activate the servos
   servoAlpha.attach(10);
   servoBeta.attach(11);
+  // set up the button
   pinMode(2, INPUT);
-  Serial.begin(9600);
-  servoAlpha.write(55);
-  servoBeta.write(55);
+  // enable serial console (currently disabled)
+  // Serial.begin(9600);
 }
 
-// forward
-
 void incrementForwardAnimation() {
+  // forward
   if (animationCycleForward < cycleMax) {
     animationCycleForward += 1;
   }
   else {
+    // when the forward animation (stage 1) is complete, change to the resting stage 2
     animationStage = 2;
   }
 }
@@ -74,13 +79,13 @@ int betaForward() {
   }
 }
 
-// backward
-
 void incrementBackAnimation() {
+  // backward
   if (animationCycleBackward < cycleMax) {
     animationCycleBackward += 1;
   }
   else {
+    // when the backward animation (stage 3) is complete, change to the resting stage 0
     animationStage = 0;
   }
 }
@@ -120,40 +125,37 @@ int betaBack() {
   }
 }
 
-// serial monitor
-
 void showValues() {
-  //char buffer[200];
-  //sprintf(buffer, " %d   %03d  %03d  %03d  %03d\n", animationStage, animationCycleForward, animationCycleBackward, servoPositionAlpha, servoPositionBeta);
-  // sprintf(buffer, " %d  animationCycleForward %03d servoPositionAlpha %03d animationCycleBackward %03d servoPositionBeta %03d\n", animationStage, animationCycleForward, servoPositionAlpha, animationCycleBackward, servoPositionBeta);
-  //Serial.write(buffer);
+  // currently disabled because it massively increases the cycle time, causing stutter
+  char buffer[200];
+  sprintf(buffer, " %d   %03d  %03d  %03d  %03d\n", animationStage, animationCycleForward, animationCycleBackward, servoPositionAlpha, servoPositionBeta);
+  Serial.write(buffer);
 }
 
 void buttonPressed() {
-  // trigger the forward animation
+  // trigger the forward animation if the current stage is zero
   if (animationStage == 0) {
     animationStage = 1;
     animationCycleForward = 0;
-    // servoAlpha.attach(10);
-    // servoBeta.attach(11);
   }
-  // trigger the backward animation
+  // trigger the backward animation if the current stage is two
   if (animationStage == 2) {
     animationStage = 3;
     animationCycleBackward = 0;
-    // servoAlpha.attach(10);
-    // servoBeta.attach(11);
   }
   // if the button is pressed while an animation is in progress (1 or 3) it is ignored
 }
 
 void checkAnimationButton() {
+  // if the button is currently unpressed, keep a record
   if (digitalRead(2) == LOW) {
     wasButtonLow = true;
   }
+  // if the button has been pressed and was not pressed on the last cycle, take action
   if (digitalRead(2) == HIGH && wasButtonLow) {
-    wasButtonLow = false;
     buttonPressed();
+    // record that the button is no longer low, to stop the action happening again while the button is pressed
+    wasButtonLow = false;
   }
 }
 
@@ -179,7 +181,7 @@ void animation(){
   // move servos
   servoAlpha.write(servoPositionAlpha);
   servoBeta.write(servoPositionBeta);
-  showValues();
+  // showValues();
 }
 
 void loop() {
